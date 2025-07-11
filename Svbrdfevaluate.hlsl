3325641475d424d29dff9086ef981c8a84a96cda
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-11 23:23:23 +0200

    Create Svbrdfevaluate.hlsl

---
+++ Svbrdfevaluate.hlsl
@@ -0,0 +1,9 @@
+//yhe svbrdf models are complex enough and huge tondeal with
+//15 MeV (i think so).
+
+//first of all, lets notice the system base valued
+//for adimensional propertied. we may be able to prevent
+//clifford algebra here. so lets dample the device pribes 
+//this will lead to another model or another esuation.
+//as this is self trusted system, lets recognize the beam type
+//by samoling probe kernels.
