71afe57b630481a949be40dd23b8798dae55493e
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-11 23:17:32 +0200

    Create AdimensionalFullEnergyheaders.h

---
+++ AdimensionalFullEnergyheaders.h
@@ -0,0 +1,6 @@
+//to compete at 15 MeV, adimensionals may not be jsefull.
+//so lets use directly clifford algebra with multiple beams
+//to hzve a soft source code here.
+//on the other hand, this repository is not suited for 650 Volts systems.
+//to add cooling support, we would use heat rransfer convection zs usual for this science.
+//but the computer system must prevent devicr crash with micro kerneld.
