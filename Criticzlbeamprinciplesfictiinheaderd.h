894e78fcee057813690bbc95d22e68149ad484a4
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-07-11 19:31:30 +0200

    Create Criticzlbeamprinciplesfictiinheaderd.h

---
+++ Criticzlbeamprinciplesfictiinheaderd.h
@@ -0,0 +1,5 @@
+//the critical system for beam cinductors 
+//must omit heat transfer in the first rquatuons of movelents.
+//those equations may be suited for km20 model.
+//as this is not cyber decurity znd we use aesrhetic functions,
+//the second order terms must be cubic.
